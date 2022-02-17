60813b1ef5701df3b318251d93a64c3d5aea9bf0
amissu89 <yonglimlee@gmail.com>
2022-02-17 11:12:15 +0900

    초기화면 5초간 뜨게함 - 액티비티 및 xml 추가 - 이미지 추가

---
+++ app/src/main/java/com/example/ourchemi/IntroActivity.java
@@ -0,0 +1,37 @@
+package com.example.ourchemi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); //뒤로가기 했을 때 다시 안나오게 >>finish!!
+                finish();
+
+            }
+
+        },5000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
